3711510ecd198aaba39cee5da36005adbca7210c
OmerutaPro <omerutapro@MacBook-Pro-de-OmerutaPro.local>
2022-09-01 12:35:43 +0200

    responsiness for main content fixed

--- script.js
+++
@@ -1,7 +0,0 @@
-const navToggle = document.querySelector(".nav-toggle");
-const links = document.querySelector(".links");
-
-//make the nav toggle button work
-navToggle.addEventListener("click", () => {
-  links.classList.toggle("show-links");
-});
